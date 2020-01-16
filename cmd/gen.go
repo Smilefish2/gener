2524967f5ec4c1f1a30e1da33c6118252b9a9977
JMJ <yang.jmj@gmail.com>
2020-01-16 15:13:47 +0800

    update

--- cmd/gen.go
+++ cmd/gen.go
@@ -12,8 +12,8 @@ import (
 var genCmd = &cobra.Command{
 	Use:     "gen",
 	Aliases: []string{"g"},
-	Short:   "生成models和proto",
-	Long:    `生成models和proto`,
+	Short:   "生成model和proto",
+	Long:    `使用多个组合命令生成model和proto文件并复制到当前目录models目录下`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -21,8 +21,18 @@ var genCmd = &cobra.Command{
 		var goPath = os.Getenv("GOPATH")
 		var goSrcPath = filepath.Join(goPath, "src")
 		var pwdPath, _ = os.Getwd()
+		var modelPath = filepath.Join(pwdPath, "models")
 		var protoPath = filepath.Join(pwdPath, "protos")
-		var templatePath = filepath.Join(goPath, "github.com/Smilefish0/gener/templates")
+		var templatePath = filepath.Join(goSrcPath, "github.com/Smilefish0/gener/templates")
+
+		// 检查protoPath目录
+		if !helpers.Exists(protoPath) {
+			err := os.Mkdir(protoPath, os.ModePerm)
+			if err != nil {
+				fmt.Printf("mkdir failed![%v]\n", err)
+				return
+			}
+		}
 
 		// 检查xo生成器命令
 		dsn := helpers.GetDatabaseDSN()
@@ -38,22 +48,21 @@ var genCmd = &cobra.Command{
 
 		// 复制models到$GOPATH/src
 		cpCmd := "cp"
-		cpArgs := []string{"-r", filepath.Join(pwdPath, "models"), filepath.Join(goSrcPath, "models")}
+		cpArgs := []string{"-r", modelPath, goSrcPath}
 		cpExec := exec.Command(cpCmd, cpArgs...)
 		_, err = cpExec.Output()
 		if err != nil {
-			//fmt.Println("xo命令未找到，请先运行: go get -u github.com/xo/xo")
 			fmt.Fprintf(os.Stderr, "The command failed to perform: %s (Command: %s, Arguments: %s)", err, cpCmd, cpArgs)
 			return
 		}
 
-		// 切换目录到$GOPATH/src
-		cmdStr := fmt.Sprintf("cd %s && proteus -p models -f models && cp models/models/* %s && rm -rf models", goSrcPath, protoPath)
+		// 切换目录到$GOPATH/src并执行一组命令
+		cmdStr := fmt.Sprintf("cd %s && proteus -p models -f models && cp models/models/generated.proto %s && rm -rf models", goSrcPath, protoPath)
 		cmdMany := exec.Command("sh", "-c", cmdStr)
-		if out, err := cmdMany.CombinedOutput(); err != nil {
+		if _, err := cmdMany.CombinedOutput(); err != nil {
 			fmt.Errorf("Error: %v\n", err)
 		} else {
-			fmt.Printf("Success: %s\n%s\n", cmdStr, out)
+			fmt.Printf("Success: \nmodel生成文件所在目录：%s\nproto生成文件所在路径:%s\n", modelPath, filepath.Join(protoPath, "generated.proto"))
 		}
 	},
 }
